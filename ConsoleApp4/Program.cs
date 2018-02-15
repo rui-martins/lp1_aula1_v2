3ee536320681a1ec101dc8f74ec64680f71ad75d
rui-martins <vandroste@gmail.com>
2018-02-15 17:01:32 +0000

    Imprimir nrs inteiros, exercicio 3

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            double numd = 1.0000;
+            float numf = 2.0000f;
+            decimal numdec = 3.0000m;
+            uint numuint = 4U;
+            int numint = 5;
+            long numlong = 6L;
+            ulong numulong = 7UL;
+
+            Console.WriteLine(numd.ToString() + "\n" + numf.ToString() + "\n" + numdec.ToString() + "\n" +
+                numuint.ToString() + "\n" + numint.ToString() + "\n" + numlong.ToString() + "\n" + numulong.ToString());           
         }
     }
 }
